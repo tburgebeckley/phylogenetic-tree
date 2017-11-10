6e4b7bbe63a1ac0018690a27a62065e697976c6b
Joseph Myers <josephlmyers@gmail.com>
2017-11-09 20:54:06 -0600

    file scanning complete

--- agglomeritiveCluster.py
+++ agglomeritiveCluster.py
@@ -11,5 +11,18 @@ else:
         print("File " + sys.argv[1] + " was successfully opened!")
     except OSError:
         print("Error!", sys.argv[1], " was not found!")
-        
+
+numSeq = int(fo.readline())
+clusterNames = []
+distances = []
+
+i = 0
+for line in fo:
+    parse = line.split( )
+    clusterNames.append(parse[0])
+    for split in parse[1:]:
+        print(split, sep=' ', end=' ')
+    print()
+
+
 #STEP 1: Cluster
